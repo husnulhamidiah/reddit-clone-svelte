16ec172cdce1c2c9122868707a316703f144f553
Anthony Ettinger <anthony@chovy.com>
2020-03-03 11:46:54 -0800

    add user rss feeds with sorting

--- src/Home.svelte
+++ src/Home.svelte
@@ -5,6 +5,7 @@
   export let username = null
   export let category = null
   let posts = []
+  let sort = '-score';
 
   $: {    
     if (category) {
@@ -19,9 +20,9 @@
   const fetchPost = async ({ username, category }) => {
     let url = 'API_BASE_URL'
 
-    if (username) url += `/user/${username}`
-    else if (category) url += `/posts/${category}`
-    else url += '/posts'
+    if (username) url += `/user/${username}?sort=${sort}`
+    else if (category) url += `/posts/${category}?sort=${sort}`
+    else url += `/posts?sort=${sort}`
 
     const res = await fetch(url)
     if (!res.ok) return alert('Something wrong!')
@@ -38,16 +39,20 @@
   })
 
   function sortBy(type = 'hot') {
-    posts = posts.sort((a, b) => {
-      if (type === 'hot') {
-        return b.score - a.score;
-      }
+    if (type === 'hot') {
+      sort = '-score'
+    } else if (type === 'new') {
+      sort = '-created'
+    } else if (type === 'comments') {
+      sort = '-score';
+    }
 
+    fetchPost({ username, category })
+    
+    posts = posts.sort((a, b) => {
       if (type === 'comments') {
         return b.comments.length - a.comments.length;
       }
-      
-      return new Date(b.created).getTime() - new Date(a.created).getTime();
     });
   }
 </script>
@@ -56,7 +61,7 @@
   <a href="#hot" on:click|preventDefault={() => sortBy('hot')}>Hot</a>
   <a href="#new" on:click|preventDefault={() => sortBy('new')}>New</a>
   <a href="#comments" on:click|preventDefault={() => sortBy('comments')}>Comments</a>
-  <a href={`/api/1/posts/${category?category+'/' : ''}rss`}>RSS</a>
+  <a href={`/api/1/${(username ? 'user' : 'posts' )}/${category || username ? (category || username)+'/' : ''}rss?sort=${sort}`}>RSS</a>
 </nav>
 {#each posts as post}
   <Post { post }></Post>
