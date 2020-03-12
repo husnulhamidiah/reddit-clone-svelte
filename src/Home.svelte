ececa8d5afeb3c6f52cc143653d6da140f5d08ae
lnappsdev <60443647+lnappsdev@users.noreply.github.com>
2020-03-12 19:55:16 +0000

    add url query sort (#45)

--- src/Home.svelte
+++ src/Home.svelte
@@ -1,12 +1,29 @@
 <script>
   import Post from './Post.svelte'
+  import { Link } from 'svelte-routing'
+  import { onMount, getContext } from 'svelte'
   import { userStore, currentCategory } from './store'
+  import {ROUTER} from 'svelte-routing/src/contexts';
+  const { activeRoute } = getContext(ROUTER);
 
   export let username = null
   export let category = null
   let posts = []
-  let sort = '-rank';
+  let sort
+  let type
   let categoryData = {}
+
+  let currentCat
+  currentCategory.subscribe(value => {
+    currentCat = value
+  })
+
+  let user
+  userStore.subscribe(value => {
+    if (value) {
+      user = value
+    }
+  })
 
   $: {    
     if (category) {
@@ -18,37 +35,10 @@
     }
   }
 
-  const fetchPost = async ({ username, category }) => {
-    let url = 'API_BASE_URL'
-
-    if (username) url += `/user/${username}?sort=${sort}`
-    else if (category) url += `/posts/${category}?sort=${sort}`
-    else url += `/posts?sort=${sort}`
-
-    const res = await fetch(url)
-    if (!res.ok) return alert('Something wrong!')
-    posts = await res.json()
-  }
-
-  const fetchCategory = async (category) => {
-    let url = 'API_BASE_URL' + `/category/${category}`
-
-    const res = await fetch(url)
-    if (!res.ok) return alert('Failed to fetch category info!')
-    categoryData = await res.json()
-  }
-
-  $: fetchPost({ username, category })
-  $: fetchCategory(category)
-
-  let user
-  userStore.subscribe(value => {
-    if (value) {
-      user = value
-    }
-  })
-
-async function sortBy(type = 'hot') {
+  const fetchPost = async ({ type, username, category }) => {
+    const urlParams = new URLSearchParams(window.location.search)
+    type = urlParams.get('sort')
+    
     if (type === 'hot') {
       sort = '-rank'
     } else if (type === 'top') {
@@ -61,14 +51,33 @@ async function sortBy(type = 'hot') {
       sort = '+score';
     }
 
-    await fetchPost({ username, category })
-    
+    let url = 'API_BASE_URL'
+
+    if (username) url += `/user/${username}?sort=${sort}`
+    else if (category) url += `/posts/${category}?sort=${sort}`
+    else url += `/posts?sort=${sort}`
+
+    const res = await fetch(url)
+    if (!res.ok) return alert('Something wrong!')
+    posts = await res.json()
+  
     posts = posts.sort((a, b) => {
       if (type === 'comments') {
         return b.commentCount - a.commentCount;
       }
     });
   }
+
+  const fetchCategory = async (category) => {
+    let url = 'API_BASE_URL' + `/category/${category}`
+
+    const res = await fetch(url)
+    if (!res.ok) return alert('Failed to fetch category info!')
+    categoryData = await res.json()
+  }
+
+  $: fetchPost({ type, username, category, $activeRoute })
+  $: fetchCategory(category)
 </script>
 
 {#if category}
@@ -79,11 +88,11 @@ async function sortBy(type = 'hot') {
 
 {/if}
 <nav class="topnav">
-  <a href="#hot" on:click|preventDefault={() => sortBy('hot')}>Hot</a>
-  <a href="#new" on:click|preventDefault={() => sortBy('new')}>New</a>
-  <a href="#top" on:click|preventDefault={() => sortBy('top')}>Top</a>
-  <a href="#comments" on:click|preventDefault={() => sortBy('comments')}>Comments</a>
-  <a href="#hot" on:click|preventDefault={() => sortBy('not')}>Controversial</a>
+  <Link to="{$activeRoute.uri}?sort=hot">Hot</Link>
+  <Link to="{$activeRoute.uri}?sort=new">New</Link>
+  <Link to="{$activeRoute.uri}?sort=top">Top</Link>
+  <Link to="{$activeRoute.uri}?sort=comments">Comments</Link>
+  <Link to="{$activeRoute.uri}?sort=not">Controversial</Link>
   <a href={`/api/1/${(username ? 'user' : 'posts' )}/${category || username ? (category || username)+'/' : ''}rss?sort=${sort}`}>RSS</a>
 </nav>
 {#each posts as post}
