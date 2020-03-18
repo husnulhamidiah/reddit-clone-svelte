f8081f9e6b4e11d6cd3ad12312f1fb3b75c688c1
lnappsdev <60443647+lnappsdev@users.noreply.github.com>
2020-03-18 23:19:51 +0000

    add comment voting and sorting (#46)

--- src/Comment.svelte
+++ src/Comment.svelte
@@ -32,6 +32,31 @@
     dispatch('update-comment', post)
   }
 
+  const voteComment = async (comment, state) => {
+    const url = `API_BASE_URL/post/${id}/${comment.id}/${state}`
+    const token = localStorage.getItem('token')
+
+    const res = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+
+    if (!res.ok) alert('Invalid credentials')
+    const data = await res.json()
+    
+    for (let i = 0; i < comments.length; i += 1) {
+      console.log(comments[i])
+      if (comments[i].id == comment.id) {
+        comments[i].score = data.score;
+        break;
+      }
+    } 
+  }
+
   let highlighted
 
   const highlightComment = async () => {
@@ -43,8 +68,28 @@
 
   onMount(() => {
     highlightComment()
+    sortComments(sort)
   })
 
+  let sort = 'new'
+
+  const sortComments = () => {
+    if (sort == 'top') {
+      comments = comments.sort((a, b) => {      
+        return b.score - a.score;
+      });
+    }
+
+    if (sort == 'new') {
+      comments = comments.sort((a, b) => {
+        b.created = new Date(b.created)
+        a.created = new Date(a.created)
+        return b.created - a.created;
+      });
+    }
+  }
+
+  $: sortComments(sort)
 
 </script>
 
@@ -67,11 +112,47 @@
   .remove-button {
     cursor: pointer;
   }
+  .content .voting-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: .3em 0;
+    font-size: 14px;
+    line-height: 1.5em;
+    font-weight: 400;
+    text-align: center;
+    justify-self: center;
+  }
+  .content .voting-container .upvote-button,
+  .content .voting-container .downvote-button {
+    transform: rotate(90deg);
+    cursor: pointer;
+  }
+  .content {
+    display: grid;
+    grid-template-columns: 4em auto;
+  }
+  .content .comment-container {
+    width: 100%;
+  }
+  .sort {
+    margin-bottom: .5em;
+  }
+
 </style>
 
+<div class="sort">
+  <a href="javascript:void(0)" on:click={() => sort = 'top'}>Top</a> ·
+  <a href="javascript:void(0)" on:click={() => sort = 'new'}>New</a>
+</div>
 <div class="content">
   {#each comments as comment}
-    <div class="comment-container" id="comment-id-{comment.id}" class:comment-highlighted={highlighted === `comment-id-${comment.id}`}>
+     <div class="voting-container">
+      <span class="upvote-button"><span on:click={ () => voteComment(comment, 'upvote') }>❮</span></span>
+      <span>{comment.score}</span>
+      <span class="downvote-button"><span on:click={ () => voteComment(comment, 'downvote') }>❯</span></span>  
+    </div>
+    <div class="comment-container" id="comment-id-{comment.id}" class:comment-highlighted={highlighted === `comment-id-${comment.id}`}>   
       <div class="comment-metadata">
         <a href={`/u/${comment.author.username}`}>{ comment.author.username }</a> · <span>{ moment(comment.created).fromNow() }</span>
         {#if comment.author.id === user.id }
